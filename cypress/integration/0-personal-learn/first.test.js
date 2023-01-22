e1f9b62f6a2839b44ba8f35df9fa9bf96b617f43
Ainafatul Nur Muslikah <ainafatul.muslikah@gmail.com>
2023-01-22 20:38:55 +0700

    first commit cypress task22

---
+++ cypress/integration/0-personal-learn/first.test.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+describe('My First Test', () => {
+    it('Visits the Kitchen Sink',() => {
+        cy.visit('https://example.cypress.io')
+        // cy.get('h1').contains('Kitchen Sink')
+        // cy.pause()
+
+        cy.contains('type').click()
+
+        //should be on a new URL which includes 'commands/actions'
+        cy.url().should('include', '/commands/actions')
+        
+        cy.get('.action-email')
+            .type('ainafatul@email.com')
+            .should('have.value','ainafatul@email.com')
+    })
+})
