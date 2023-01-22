8e95696cf44fd0b80049961a4f75e58733fd88c0
Ainafatul Nur Muslikah <ainafatul.muslikah@gmail.com>
2023-01-23 00:31:35 +0700

    Interacting and Login

---
+++ cypress/integration/0-personal-learn/interacting.test.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe('Browser Actions', () => {
+    it('Should load books website', () => {
+        cy.visit('https://books.toscrape.com/index.html', {timeout:10000})
+        cy.url().should('include','index.html')
+    });
+
+    it('Should click on Travel category', () => {
+        cy.get('a').contains('Travel').click()
+        cy.get('h1').contains('Travel')
+        // cy.get('h1').should('have.text','Travel')
+    });
+
+})
---
+++ cypress/integration/0-personal-learn/login.test.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+describe('Working with inputs', () => {
+
+    it('Visit the website', () => {
+        cy.visit('http://zero.webappsecurity.com/login.html')
+        cy.url().should('include','login.html')
+    });
+
+    // get pake ID = # , Class = . 
+    it('Should fill username', () => {
+        cy.get('#user_login').clear()
+        cy.get('#user_login').type('username')
+    });
+
+    // get pake name
+    it('Should fill password', () => {
+        cy.get('input[name="user_password"]').clear()
+        cy.get('input[name="user_password"]').type('password')
+    });
+
+    
+})
