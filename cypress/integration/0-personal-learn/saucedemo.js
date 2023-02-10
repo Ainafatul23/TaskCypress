1c1207a46b282e9f9c41002f736ea12e20830db3
Ainafatul Nur Muslikah <ainafatul.muslikah@gmail.com>
2023-02-11 00:17:50 +0700

    user fixtures

---
+++ cypress/integration/0-personal-learn/saucedemo.js
@@ -0,0 +1,52 @@
+/// <reference types="cypress" />
+
+describe('Working with inputs', () => {
+
+    it('Visit the website', () => {
+        cy.visit('https://www.saucedemo.com/')
+    });
+
+    it('Should Try Login', () => {
+        cy.fixture("user").then(user => {
+            const username = user.username
+            const password = user.password
+
+            cy.get('#user-name').clear()
+            cy.get('#user-name').type(username)
+
+            cy.get('#password').clear()
+            cy.get('#password').type(password)
+
+            cy.get('#login-button').click()
+            
+        })
+    });
+
+    it('Should Try Checkout', () => {
+       cy.fixture("checkout").then(checkout => {
+        const firstname = checkout.firstname
+        const lastname = checkout.lastname
+        const zip = checkout.zip
+
+        cy.get('#add-to-cart-sauce-labs-backpack').click()
+        cy.wait(1000)
+
+        cy.get('#shopping_cart_container').click()
+
+        cy.get("#checkout").click()
+
+        cy.get('#first-name').clear()
+        cy.get('#first-name').type(firstname)
+
+        cy.get('#last-name').clear()
+        cy.get('#last-name').type(lastname)
+
+        cy.get('#postal-code').clear()
+        cy.get('#postal-code').type(zip)
+
+        cy.get('#finish').click()
+    })
+    });
+
+    
+})
